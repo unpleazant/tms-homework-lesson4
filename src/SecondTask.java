43f5dde62bc3162e2fbd249cd5e7765c5ddcbfbb
Evgenii <15147472+unpleazant@users.noreply.github.com>
2021-04-21 11:26:13 +0300

    Update SecondTask.java

--- src/SecondTask.java
+++ src/SecondTask.java
@@ -23,8 +23,14 @@ public class SecondTask extends TaskHelper {
         String[][] chess = new String[8][8];
 
         for (int i = 0; i < 8; i++) {
-            if (i % 2 == 0) for (int j = 0; j < 8; j++) chess[i][j] = (j % 2 == 0) ? "W" : "B";
-            else for (int j = 0; j < 8; j++) chess[i][j] = (j % 2 == 0) ? "B" : "W";
+            if (i % 2 == 0) {
+                for (int j = 0; j < 8; j++) {
+                    chess[i][j] = (j % 2 == 0) ? "W" : "B";
+                }
+            }
+            else for (int j = 0; j < 8; j++) {
+                chess[i][j] = (j % 2 == 0) ? "B" : "W";
+            }
         }
         stream(chess).forEach(row -> System.out.println(Arrays.deepToString(row)));
     }
